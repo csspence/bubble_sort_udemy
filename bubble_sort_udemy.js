5017858b0397dab0c82a9f6ec202b320b32d107d
Crew Spence <csspence123@gmail.com>
2020-07-30 06:05:00 -0500

    Wrote working solution.

--- bubble_sort_udemy.js
+++ bubble_sort_udemy.js
@@ -4,5 +4,16 @@ Bubble Sort
 */
 
 const bubbleSort = (arr) => {
+  const swap = (array, ind1, ind2) => {
+    [array[ind1], array[ind2]] = [array[ind2], array[ind1]];
+  }
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = i + 1; j < arr.length; j++) {
+      if(arr[i] > arr[j]) {
+        swap(arr, i, j);
+      }
+    }
+  }
 
+  return arr;
 }
